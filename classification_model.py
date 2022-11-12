a3cdae1cf351df13f1dde6db056681662693e9bc
Davide Wiest <davide.wiest2@gmail.com>
2022-11-12 12:54:11 +0100

    add loss fn and optimizer on cm model

--- classification_model.py
+++ classification_model.py
@@ -22,8 +22,12 @@ circles = pd.DataFrame({"x1": x[:, 0], "x2": x[:, 1], "label": y})
 x = torch.from_numpy(x).type(torch.float)
 y = torch.from_numpy(y).type(torch.float)
 
-x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
+# x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+test_sample = round(len(x) * 0.8)
+x_train = x[:test_sample]
+y_train = y[:test_sample]
+x_test = x[test_sample:]
+y_test = y[test_sample:]
 
 class circleModel(nn.Module):
     def __init__(self):
@@ -46,4 +50,23 @@ cm = nn.Sequential(
 with torch.inference_mode():
     untrained_preds = cm(x_train.to(device))
     # print(untrained_preds)
+
+loss_fn = nn.BCEWithLogitsLoss() # sigmoid activation function built in
+# normal BCE with loss requires inputs to have gone through sigmoid activation function prior
+
+optimizer = torch.optim.SGD(params=cm.paramters(), lr=0.1)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
+
+
+
+
+
+
+
+
+
 
